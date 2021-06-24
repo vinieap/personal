ebd15129836dc2f18f39ea530010efc86b85c001
Vinit Patel <vinieap@gmail.com>
2021-06-24 17:27:21 -0400

    idk

---
+++ build/precache-manifest.d5cce5b7c9741b2d97b79be1ae6deff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f2dc2c2f9edd3eb125a",
+    "url": "/personal/static/css/main.cc477d97.chunk.css"
+  },
+  {
+    "revision": "4f2dc2c2f9edd3eb125a",
+    "url": "/personal/static/js/main.05f82a2b.chunk.js"
+  },
+  {
+    "revision": "cdddc6f9b899f4689d49",
+    "url": "/personal/static/js/runtime~main.a96717cf.js"
+  },
+  {
+    "revision": "92d15c731a5628d078cf",
+    "url": "/personal/static/js/2.96d5101c.chunk.js"
+  },
+  {
+    "revision": "494f9b1c08ca29b9020b",
+    "url": "/personal/static/js/3.067b173f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/personal/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c8948df980ed70d02d86d6e5c22c38c9",
+    "url": "/personal/index.html"
+  }
+];
